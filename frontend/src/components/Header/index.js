b1cdbb8ceecb0c721e9a6843a55a9cf4f49aa7c3
nghthu <hanhh.thu@gmail.com>
2023-11-30 22:51:34 +0700

    language button and logout

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,12 +1,12 @@
-import { Button, Dropdown } from 'antd';
-import { LogoutOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Space } from 'antd';
+import { LogoutOutlined, DownOutlined, GlobalOutlined } from '@ant-design/icons';
 import classnames from 'classnames/bind';
 import styles from '@/styles/components/Header.module.scss';
 import HomeButton from '@/components/HomeButton';
 
 const cx = classnames.bind(styles);
 
-const items = [
+const userItems = [
   {
     label: <a>My Classes</a>,
     key: '0',
@@ -19,9 +19,20 @@ const items = [
     type: 'divider',
   },
   {
-    label: 'Log Out',
+    label: <a href="/api/auth/logout">Log Out</a>,
     key: '3',
     icon: <LogoutOutlined />,
+  },
+];
+
+const languageItems = [
+  {
+    label: 'English',
+    key: '0',
+  },
+  {
+    label: 'Tiếng Việt',
+    key: '1',
   },
 ];
 
@@ -43,9 +54,10 @@ export default function Header({ user }) {
         {user && (
           <Dropdown
             menu={{
-              items,
+              items: userItems,
             }}
             trigger={['click']}
+            placement="bottomRight"
           >
             <a
               className={cx('avatar')}
@@ -58,6 +70,23 @@ export default function Header({ user }) {
             </a>
           </Dropdown>
         )}
+        <Dropdown
+          menu={{
+            items: languageItems,
+          }}
+          trigger={['click']}
+        >
+          <a
+            className={cx('language')}
+            onClick={(e) => e.preventDefault()}
+          >
+            <Space>
+              <GlobalOutlined />
+              Language
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
       </div>
     </header>
   );
