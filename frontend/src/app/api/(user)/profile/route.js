9732023978cb0ddb8da017dccb49f5a51c8f807d
Nguyen Ngoc Thuy <nnthuy159@gmail.com>
2023-12-03 16:06:44 +0700

    fix home logging error

--- frontend/src/app/api/(user)/profile/route.js
+++ frontend/src/app/api/(user)/profile/route.js
@@ -3,17 +3,18 @@ import { getAccessToken } from '@auth0/nextjs-auth0';
 import { NextResponse } from 'next/server';
 
 export const GET = async function profileGetRoute(req) {
-  const { accessToken } = await getAccessToken();
-  if (!accessToken) {
-    return NextResponse.json({ error: ERROR_NOT_AUTHENTICATED }, { status: 401 });
+  try {
+    const { accessToken } = await getAccessToken();
+
+    const response = await fetch(`${process.env.API_BASE_URL}/api/user`, {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const data = await response.json();
+    return NextResponse.json({ user: data.user });
+  } catch (err) {
+    return NextResponse.json({ error: ERROR_NOT_AUTHENTICATED }, { status: 200 });
   }
-
-  const response = await fetch(`${process.env.API_BASE_URL}/api/user`, {
-    headers: {
-      authorization: `Bearer ${accessToken}`,
-    },
-  });
-
-  const data = await response.json();
-  return NextResponse.json({ user: data.user });
 };
