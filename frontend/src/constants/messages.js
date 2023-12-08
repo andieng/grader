725deb6d80d1569429b270c3cdf7bf03c1fd81ee
Nguyen Ngoc Thuy <nnthuy159@gmail.com>
2023-12-08 23:58:20 +0700

    admin & user role

--- frontend/src/constants/messages.js
+++ frontend/src/constants/messages.js
@@ -1,2 +1,3 @@
 export const ERROR_NOT_VERIFIED = 'Sorry, you need to verify your account to access this page';
 export const ERROR_NOT_AUTHENTICATED = 'Not authenticated';
+export const ERROR_ROLE_NOT_FOUND = 'Role not found';
