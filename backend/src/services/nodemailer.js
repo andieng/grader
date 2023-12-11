82f4b9a4efca71eef14745f9bf7c8f47528ce1f3
Nguyen Ngoc Thuy <nnthuy159@gmail.com>
2023-12-11 23:57:05 +0700

    test send email deploy

--- backend/src/services/nodemailer.js
+++ backend/src/services/nodemailer.js
@@ -25,19 +25,35 @@ export async function sendMail(mailContent) {
     text: generateInvitationPlainText(mailContent),
     html: generateInvitationHtml(mailContent),
   };
-  await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailOptions, (err, info) => {
-      transporter.close();
-      if (err) {
-        console.error(err);
+  return new Promise((resolve, reject) => {
+    transporter
+      .sendMail(mailOptions)
+      .then((info) => {
+        if (info) {
+          console.log("Email sent: ", info.messageId);
+          resolve();
+        } else {
+          resolve(null);
+        }
+      })
+      .catch((err) => {
+        console.error("Email error:::", err);
         reject(err);
-      } else {
-        console.log(info);
-        resolve(info);
-      }
-    });
+      });
   });
+  // await new Promise((resolve, reject) => {
+  //   // send mail
+  //   transporter.sendMail(mailOptions, (err, info) => {
+  //     transporter.close();
+  //     if (err) {
+  //       console.error(err);
+  //       reject(err);
+  //     } else {
+  //       console.log(info);
+  //       resolve(info);
+  //     }
+  //   });
+  // });
   // transporter.sendMail(mailOptions, (error, info) => {
   //   transporter.close();
   //   if (error) {
