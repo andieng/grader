1e10fb5d3d2c72e7a03e1fb4bdde20b067592eb1
Nguyen Ngoc Thuy <nnthuy159@gmail.com>
2024-01-13 18:11:23 +0700

    Fix syntax error

--- backend/src/models/Assignment.js
+++ backend/src/models/Assignment.js
@@ -3,62 +3,7 @@ const { Model, Sequelize } = _sequelize;
 
 export default class Assignment extends Model {
   static init(sequelize, DataTypes) {
-    return super.init({
-      assignmentId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        field: 'assignment_id'
-      },
-      classId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        references: {
-          model: 'classes',
-          key: 'class_id'
-        },
-        field: 'class_id'
-      },
-      assignmentName: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-        field: 'assignment_name'
-      },
-      assignmentGradeScale: {
-        type: DataTypes.DECIMAL,
-        allowNull: true,
-        field: 'assignment_grade_scale'
-      },
-      isPublished: {
-        type: DataTypes.BOOLEAN,
-        allowNull: true,
-        field: 'is_published'
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.Sequelize.fn('now'),
-        field: 'created_at'
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.Sequelize.fn('now'),
-        field: 'updated_at'
-      },
-      lineNumber: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        field: 'line_number'
-      }
-    }, {
-      sequelize,
-      tableName: 'assignments',
-      schema: 'public',
-      hasTrigger: true,
-      timestamps: false,
-      indexes: [
+    return super.init(
       {
         assignmentId: {
           type: DataTypes.UUID,
--- backend/src/models/GradePublication.js
+++ backend/src/models/GradePublication.js
@@ -3,56 +3,7 @@ const { Model, Sequelize } = _sequelize;
 
 export default class GradePublication extends Model {
   static init(sequelize, DataTypes) {
-    return super.init({
-      gradePublicationId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        field: 'grade_publication_id'
-      },
-      assignmentId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        field: 'assignment_id'
-      },
-      classId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        references: {
-          model: 'classes',
-          key: 'class_id'
-        },
-        field: 'class_id'
-      },
-      teacherUserId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        field: 'teacher_user_id'
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.Sequelize.fn('now'),
-        field: 'created_at'
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.Sequelize.fn('now'),
-        field: 'updated_at'
-      }
-    }, {
-      sequelize,
-      tableName: 'grade_publications',
-      schema: 'public',
-      hasTrigger: true,
-      timestamps: false,
-      indexes: [
+    return super.init(
       {
         gradePublicationId: {
           type: DataTypes.UUID,
